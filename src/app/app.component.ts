cb4e6764c8e7490e693845c41af52a15d2a3987d
Quang Truong <truong.bq00118@sinhvien.hoasen.edu.vn>
2023-02-17 09:30:30 +0700

    Edit router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth, user } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 import { onAuthStateChanged } from '@firebase/auth';
 import { AuthService } from './services/auth.service';
 import { NewsService } from './services/news.service';
@@ -12,11 +13,16 @@ import { NewsService } from './services/news.service';
 export class AppComponent implements OnInit {
   title = 'HttpClient';
 
-  constructor(private auth: Auth, private authService: AuthService) {}
+  constructor(
+    private auth: Auth,
+    private authService: AuthService,
+    private router: Router
+  ) {}
   ngOnInit() {
     onAuthStateChanged(this.auth, (user) => {
       if (user) {
         this.authService.currentUser = user;
+        this.router.navigateByUrl('/home');
       } else {
         console.log('No user!');
         this.authService.currentUser = null;
